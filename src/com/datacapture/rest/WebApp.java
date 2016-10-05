96f82a497893837aee846a2586d2a5410abed91d
JensErikJagdHansen <jeh@jagd-hansen.dk>
2016-10-05 21:00:38 +0200

    Change in merge of Will affect only plan_actual in statistics Also started on new api for qi-flow

--- src/com/datacapture/rest/WebApp.java
+++ src/com/datacapture/rest/WebApp.java
@@ -1,7 +1,7 @@
 package com.datacapture.rest;
 
 
-// Version 2016.09.21 14.30
+// Version 2016.10.05 14.30
 
 
 import java.sql.Connection;
@@ -789,10 +789,38 @@ public class WebApp {
 				JSONHelper.json_db("q",strSQL_sequences_defecttypes, 0).toString(1)).build();}
 	
 	
+	
 	@Path("/sequence/rework/{DefectTypeID}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response return_sequence_rework(@PathParam("DefectTypeID") String strDefectTypeID) throws Exception {
+
+		// find object for for the defect type
+		JSONObject jo_out  =  JSONHelper.json_db("q",strSQL_sequences_defecttype, 1, strDefectTypeID).getJSONObject(0);
+			
+		// find sequence from item ID. Look up in table "420_rework_sequences" 
+		JSONArray ja  =  JSONHelper.json_db("q",strSQL_sequences_rework, 1, strDefectTypeID);
+
+		
+		// Find the product information. Put into object ja_out. Look up in table "310_Product"  
+		if (ja.length()>0 ) {
+
+			jo_out.put("Steps", ja);
+			return Response.ok(jo_out.toString(1)).build();}
+		
+		// return error code if not found
+		JSONArray Msg = JSONHelper.json_db("q",strSQL_ErrMsg, 1 ,"sequence_not_found_rework");	
+		String str = Msg.getJSONObject(0).toString(1).replace("??", strDefectTypeID);
+		return Response.status(404).entity(str).build();
+	}
+	
+	
+	//jens
+	
+	@Path("/sequence/defecttypes/item/{ItemID}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response return_defecttypes_item(@PathParam("ItemID") String strDefectTypeID) throws Exception {
 
 		// find object for for the defect type
 		JSONObject jo_out  =  JSONHelper.json_db("q",strSQL_sequences_defecttype, 1, strDefectTypeID).getJSONObject(0);
@@ -1271,7 +1299,7 @@ public class WebApp {
 //-------------------------------------------------------------------------------------------------------------------------------------------------------	
 // Support functions
 	
-	// Help funktions
+	// Help functions
 
 	private static class Basket_Helper {
 		
@@ -1470,8 +1498,9 @@ public class WebApp {
 			for (int i=0;i<Target.length();i++) {
 				for (int j=0;j<Source.length();j++){
 					JSONObject jo = Source.getJSONObject(j);
+
 					
-					if ( Target.getJSONObject(i).optInt(key) ==  jo.optInt(key) ) {
+					if ( Target.getJSONObject(i).opt(key) ==  jo.opt(key) ) {
 						for(String key2 : JSONObject.getNames(jo))
 						{
 							Target.getJSONObject(i).put(key2, jo.get(key2));
