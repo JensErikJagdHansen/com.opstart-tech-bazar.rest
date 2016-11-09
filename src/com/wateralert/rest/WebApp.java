1255ee14b8492805a23bc72b6c460a5cec8d51a7
JensErikJagdHansen <jeh@jagd-hansen.dk>
2016-11-09 18:59:05 +0100

    Remove old code

---
+++ src/com/wateralert/rest/WebApp.java
@@ -0,0 +1,37 @@
+package com.wateralert.rest;
+
+// Version 2016.09.21 14.30
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import static java.util.concurrent.TimeUnit.*;
+
+
+@Path("/v1") 
+public class WebApp {
+	
+	//Connection version 
+	public static final Integer intPrint_JSON = 0 ; //Set =0 if not print JSON, =1 if print JSON
+	public static final String dbURL = "jdbc:sqlserver://implement-dev.com:49170;databaseName=wateralert;user=flowline;password=123";
+
+	private static final JSONHelper JSONHelper = new JSONHelper(dbURL,intPrint_JSON);
+		
+	
+}
+
+
+
+
+
+	
+	
+
+
