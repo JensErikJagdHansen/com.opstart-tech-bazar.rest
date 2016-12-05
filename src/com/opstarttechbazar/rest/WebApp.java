d273bbc0bfc6274924ed9fefb7f3d6613e1acc90
JensErikJagdHansen <jeh@jagd-hansen.dk>
2016-12-05 23:22:44 +0100

    first test

--- src/com/opstarttechbazar/rest/WebApp.java
+++ src/com/opstarttechbazar/rest/WebApp.java
@@ -1,6 +1,6 @@
 package com.opstarttechbazar.rest;
 
-// Version 2016.09.21 14.30
+// Version 2016.12.05 14.30
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -11,6 +11,9 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.json.JSONObject;
+
+import com.opstarttechbazar.rest.JSONHelper;
+
 import org.json.JSONArray;
 import static java.util.concurrent.TimeUnit.*;
 
@@ -21,10 +24,45 @@ public class WebApp {
 	//Connection version 
 	public static final Integer intPrint_JSON = 0 ; //Set =0 if not print JSON, =1 if print JSON
 
-	public static final String dbURL = "jdbc:sqlserver://localhost:49170;databaseName=wateralert;user=flowline;password=123";
+	public static final String dbURL = "jdbc:sqlserver://localhost:49170;databaseName=opstarttechbazar;user=flowline;password=123";
+
 	
 	private static final JSONHelper JSONHelper = new JSONHelper(dbURL,intPrint_JSON);
+	
+	private static final String strSQL_insert_beacon_log =  "Insert Into beacon_log Values (?,?,?,?, ?,?,?,?, ?)";	
+	
+	@Path("/log")
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response log_data_a(String strLogData) throws Exception { 
+
 		
+		String strDevice = "a";
+		// Parse the string to json object
+		JSONArray ja_LogData = new JSONArray(strLogData);
+		System.out.println(ja_LogData.toString(1));
+		
+		JSONObject jo = new JSONObject();
+/*		
+		
+		for (int i = 0; i <  ja_LogData.length(); i++) {
+				jo=ja_LogData.getJSONObject(i);
+				JSONHelper.json_db("e", strSQL_insert_beacon_log ,9,jo.opt("RSSI"), 
+																	jo.opt("Major/Instance"),
+																	jo.opt("TX"),
+																	jo.opt("UUID/Namespace"),
+																	jo.opt("Minor"),
+																	jo.opt("Timestamp"),
+																	jo.opt("TimeFormatted"),
+																	jo.opt("Distance"),
+																	strDevice); 
+		}
+	*/	
+		return Response.ok("test").build();
+	}
+
+	
 	
 	
 }
