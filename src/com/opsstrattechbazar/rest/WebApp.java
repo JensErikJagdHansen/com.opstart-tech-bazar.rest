bc274de7b8b570d429bd35587fbf15f676d06b61
JensErikJagdHansen <jeh@jagd-hansen.dk>
2016-12-06 00:57:44 +0100

    running

--- src/com/opsstrattechbazar/rest/WebApp.java
+++ src/com/opsstrattechbazar/rest/WebApp.java
@@ -21,7 +21,7 @@ public class WebApp {
 	//Connection version 
 	public static final Integer intPrint_JSON = 0 ; //Set =0 if not print JSON, =1 if print JSON
 
-	public static final String dbURL = "jdbc:sqlserver://localhost:49170;databaseName=opstarttechbazar;user=flowline;password=123";
+	public static final String dbURL = "jdbc:sqlserver://localhost:49170;databaseName=opsstrattechbazar;user=flowline;password=123";
 	//Connection version 
 	public static final String strVersion = "2.0.4.16d";
 
@@ -29,7 +29,9 @@ public class WebApp {
 	
 	private static final JSONHelper JSONHelper = new JSONHelper(dbURL,intPrint_JSON);
 	
-	private static final String strSQL_insert_beacon_log =  "Insert Into beacon_log Values (?,?,?,?, ?,?,?,?, ?)";	
+	private static final String strSQL_insert_beacon_log =  "Insert Into [110_Beacon_scan] Values (?,?,?, ?,?,?, ?, getutcdate() )";	
+	
+	private static final String strSQL_delete_beacon_log =  "delete from [110_Beacon_scan] where UserID = ? ";
 	
 	
 	@Path("/version")
@@ -39,7 +41,7 @@ public class WebApp {
 
 	
 	
-	
+
 	@Path("/log")
 	@POST
 	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
@@ -47,27 +49,26 @@ public class WebApp {
 	public Response log_data_a(String strLogData) throws Exception { 
 
 		
-		String strDevice = "a";
+		String strUserID = "JEH";
 		// Parse the string to json object
 		JSONArray ja_LogData = new JSONArray(strLogData);
 		System.out.println(ja_LogData.toString(1));
 		
 		JSONObject jo = new JSONObject();
-/*		
+		
 		
 		for (int i = 0; i <  ja_LogData.length(); i++) {
 				jo=ja_LogData.getJSONObject(i);
-				JSONHelper.json_db("e", strSQL_insert_beacon_log ,9,jo.opt("RSSI"), 
+				JSONHelper.json_db("e", strSQL_insert_beacon_log ,7, 																	
+																	jo.get("UUID/Namespace"),
 																	jo.opt("Major/Instance"),
-																	jo.opt("TX"),
-																	jo.opt("UUID/Namespace"),
 																	jo.opt("Minor"),
-																	jo.opt("Timestamp"),
-																	jo.opt("TimeFormatted"),
+																	jo.opt("RSSI"),
+																	jo.opt("TX"),
 																	jo.opt("Distance"),
-																	strDevice); 
+																	strUserID); 
 		}
-	*/	
+		
 		return Response.ok("test").build();
 	}
 
